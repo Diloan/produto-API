d2d2919156da9df32733077921c04f1c0514c3d8
Diloan <diloan.silva@gmail..com>
2023-02-26 15:58:36 -0300

    Adicionando interface nos serviços para desacoprar/separar responsabilidades das classes. - Ajustandos alguns métodos

--- src/main/java/com/mvp/produtoapi/controller/PedidoController.java
+++ src/main/java/com/mvp/produtoapi/controller/PedidoController.java
@@ -6,7 +6,7 @@ import com.mvp.produtoapi.exception.PedidoIllegalArgumentException;
 import com.mvp.produtoapi.exception.PedidoInternalServerErrorException;
 import com.mvp.produtoapi.exception.PedidoNotFoundException;
 import com.mvp.produtoapi.exception.ProdutoNotFoundException;
-import com.mvp.produtoapi.service.PedidoService;
+import com.mvp.produtoapi.service.PedidoServiceImp;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -24,17 +24,17 @@ import java.util.UUID;
 @RequestMapping("/pedidos")
 public class PedidoController {
 
-    private final PedidoService pedidoService;
+    private final PedidoServiceImp pedidoServiceImp;
 
     @Autowired
-    public PedidoController(PedidoService pedidoService) {
-        this.pedidoService = pedidoService;
+    public PedidoController(PedidoServiceImp pedidoServiceImp) {
+        this.pedidoServiceImp = pedidoServiceImp;
     }
 
     @PostMapping("/adicionar")
     @Transactional
     public ResponseEntity<PedidoDTO> cadastrarPedido(@RequestBody PedidoDTO pedidoDTO, UriComponentsBuilder uriBuilder) {
-        Pedido pedido = pedidoService.cadastrarPedido(pedidoDTO);
+        Pedido pedido = pedidoServiceImp.cadastrarPedido(pedidoDTO);
 
         if (pedido == null) {
             throw new PedidoInternalServerErrorException("Erro ao cadastrar pedido");
@@ -46,7 +46,7 @@ public class PedidoController {
 
     @GetMapping
     public ResponseEntity<List<PedidoDTO>> listarTodosPedidos(@RequestParam int pagina, @RequestParam int quantidade, @RequestParam String ordenacao, @RequestParam String direcao) {
-        Page<Pedido> pedidos = pedidoService.listarTodosPedidos(pagina, quantidade, ordenacao, direcao);
+        Page<Pedido> pedidos = pedidoServiceImp.listarTodosPedidos(pagina, quantidade, ordenacao, direcao);
 
         if (pedidos.isEmpty()) {
             throw new ProdutoNotFoundException("Nenhum pedido encontrado");
@@ -58,7 +58,7 @@ public class PedidoController {
 
     @GetMapping("/{id}")
     public ResponseEntity<PedidoDTO> buscarPedidoPorId(@PathVariable UUID id) {
-        Optional<Pedido> pedidos = pedidoService.buscarPedidoPorId(id);
+        Optional<Pedido> pedidos = pedidoServiceImp.buscarPedidoPorId(id);
 
         if (pedidos.isEmpty()) {
             throw new PedidoNotFoundException("Pedido não encontrado");
@@ -70,26 +70,26 @@ public class PedidoController {
     @PatchMapping("{id}")
     @Transactional
     public ResponseEntity<PedidoDTO> atualizarPedido(@PathVariable UUID id , @RequestBody PedidoDTO pedidoDTO) {
-        Optional<Pedido> pedido = pedidoService.buscarPedidoPorId(id);
+        Optional<Pedido> pedido = pedidoServiceImp.buscarPedidoPorId(id);
 
         if (pedido.isEmpty()) {
             throw new PedidoNotFoundException("Pedido não encontrado");
         }
 
-        pedidoService.atualizarPedido(pedidoDTO);
-        return ResponseEntity.ok(pedidoDTO);
+        Pedido pedidoAtualizado = pedidoServiceImp.atualizarPedido(pedidoDTO);
+        return ResponseEntity.ok(PedidoDTO.converterToDto(pedidoAtualizado));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity<Void> removerPedido(@PathVariable UUID id) {
-        Optional<Pedido> Pedido = pedidoService.buscarPedidoPorId(id);
+        Optional<Pedido> Pedido = pedidoServiceImp.buscarPedidoPorId(id);
 
         if (Pedido.isEmpty()) {
             throw new PedidoNotFoundException("Pedido não encontrado");
         }
 
-        pedidoService.removePedido(id);
+        pedidoServiceImp.removerPedido(id);
         return ResponseEntity.noContent().build();
     }
 
@@ -97,7 +97,7 @@ public class PedidoController {
     @Transactional
     public ResponseEntity<PedidoDTO> aplicarDesconto(@PathVariable UUID id, @PathVariable BigDecimal desconto) {
 
-        Optional<Pedido> pedido = pedidoService.buscarPedidoPorId(id);
+        Optional<Pedido> pedido = pedidoServiceImp.buscarPedidoPorId(id);
         if (pedido.isEmpty()) {
             throw new PedidoNotFoundException("Pedido não encontrado");
         }
@@ -108,7 +108,7 @@ public class PedidoController {
             throw new PedidoIllegalArgumentException("Desconto não pode ser maior que 100%");
         }
 
-        PedidoDTO pedidoDTO = pedidoService.aplicarDesconto(pedido.get(), desconto);
+        PedidoDTO pedidoDTO = pedidoServiceImp.aplicarDesconto(pedido.get(), desconto);
 
         return ResponseEntity.ok(pedidoDTO);
     }
--- src/main/java/com/mvp/produtoapi/controller/ProdutoController.java
+++ src/main/java/com/mvp/produtoapi/controller/ProdutoController.java
@@ -5,7 +5,7 @@ import com.mvp.produtoapi.entity.Produto;
 import com.mvp.produtoapi.exception.ProdutoBadRequestException;
 import com.mvp.produtoapi.exception.ProdutoInternalServerErrorException;
 import com.mvp.produtoapi.exception.ProdutoNotFoundException;
-import com.mvp.produtoapi.service.ProdutoService;
+import com.mvp.produtoapi.service.ProdutoServiceImp;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -22,27 +22,27 @@ import java.util.UUID;
 @RequestMapping("/produtos")
 public class ProdutoController {
 
-    private final ProdutoService produtoService;
+    private final ProdutoServiceImp produtoServiceImp;
 
     @Autowired
-    public ProdutoController(ProdutoService produtoService) {
-        this.produtoService = produtoService;
+    public ProdutoController(ProdutoServiceImp produtoServiceImp) {
+        this.produtoServiceImp = produtoServiceImp;
     }
 
     @PostMapping("/adicionar")
     @Transactional
     public ResponseEntity<ProdutoDTO> cadastrarProduto(@RequestBody ProdutoDTO produtoDTO, UriComponentsBuilder uriBuilder) {
-        Produto produto = produtoService.cadastrarProduto(produtoDTO);
+        Produto produto = produtoServiceImp.cadastrarProduto(produtoDTO);
         if (produto == null) {
             throw new ProdutoInternalServerErrorException("Erro ao cadastrar produto");
         }
         URI uri = uriBuilder.path("produtos/{id}").buildAndExpand(produto.getId()).toUri();
-        return ResponseEntity.created(uri).body(produtoDTO.converterToDto(produto));
+        return ResponseEntity.created(uri).body(ProdutoDTO.converterToDto(produto));
     }
 
     @GetMapping
     public ResponseEntity<List<ProdutoDTO>> listarTodosProdutos(@RequestParam int pagina, @RequestParam int quantidade, @RequestParam String ordenacao, @RequestParam String direcao) {
-        Page<Produto> produtos = produtoService.listarTodosProdutos(pagina, quantidade, ordenacao, direcao);
+        Page<Produto> produtos = produtoServiceImp.listarTodosProdutos(pagina, quantidade, ordenacao, direcao);
         if (produtos.isEmpty()) {
             throw new ProdutoNotFoundException("Nenhum produto encontrado");
         }
@@ -52,7 +52,7 @@ public class ProdutoController {
 
     @GetMapping("/{id}")
     public ProdutoDTO buscarProdutoPorId(@PathVariable UUID id) {
-        Optional<Produto> produtos = produtoService.buscarProdutoPorId(id);
+        Optional<Produto> produtos = produtoServiceImp.buscarProdutoPorId(id);
 
         if (produtos.isEmpty()) {
             throw new ProdutoNotFoundException(id);
@@ -64,31 +64,31 @@ public class ProdutoController {
     @PatchMapping("{id}")
     @Transactional
     public ResponseEntity<ProdutoDTO> atualizarProduto(@PathVariable UUID id , @RequestBody ProdutoDTO produtoDTO) {
-        Optional<Produto> produto = produtoService.buscarProdutoPorId(id);
+        Optional<Produto> produto = produtoServiceImp.buscarProdutoPorId(id);
 
         if (produto.isEmpty()) {
             throw new ProdutoNotFoundException(id);
         }
 
-        produtoService.atualizarProduto(produtoDTO);
-        return ResponseEntity.ok(produtoDTO);
+        Produto produtoAtualizado = produtoServiceImp.atualizarProduto(produtoDTO);
+        return ResponseEntity.ok(ProdutoDTO.converterToDto(produtoAtualizado));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity<String> removerProduto(@PathVariable UUID id) {
-        Optional<Produto> produto = produtoService.buscarProdutoPorId(id);
+        Optional<Produto> produto = produtoServiceImp.buscarProdutoPorId(id);
 
         if (produto.isEmpty()) {
             throw new ProdutoNotFoundException(id);
         }
 
-        Boolean estaEmPedido = produtoService.verificarSeProdutoEstaEmPedido(id);
+        Boolean estaEmPedido = produtoServiceImp.verificarSeProdutoEstaEmPedido(id);
         if (estaEmPedido) {
             throw new ProdutoBadRequestException(id, "Produto não pode ser removido pois está em um pedido, id: ");
         }
 
-        produtoService.removeProduto(id);
+        produtoServiceImp.removerProduto(id);
         return ResponseEntity.noContent().build();
     }
 
